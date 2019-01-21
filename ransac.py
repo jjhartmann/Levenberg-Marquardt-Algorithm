c097624a6c338eee395f09fcee5bc44b434c0b6d
Jeremy J. Hartmann <jeremy.hartmann@gmail.com>
2019-01-21 01:32:35 -0500

    Update ransac implementation

--- ransac.py
+++ ransac.py
@@ -1,27 +1,61 @@
-
+""" Module: RANSAC Implementations
+    Used for optimizing paramters to fit a distribtion using random sampling.
+"""
 import numpy as np
+import random
 from numpy import inner, max, diag, eye, Inf, dot
 from numpy.linalg import norm, solve
 import time
 
+def stub1(test, data):
+    pass
+
+def stub2(data, params, epsilon):
+    pass
+
+
 class RansacModel:
-    _minModelParams=2
-    _createModelCallback = None
-    _evaluateModelCallback = None
-    _params = None
-    def __init__(self, minParams, createModelCallback, evaluateModelCallback):
-        _minModelparams=minParams
-        _createModelCallback = createModelCallback
-        _evaluateModelCallback = evaluateModelCallback
+    def __init__(self, data, create_model_callback, evaluate_model_callback, epsilon=2):
+        self._createModelCallback = create_model_callback
+        self._evaluateModelCallback = evaluate_model_callback
+        self._data = data
+        self._epsilon = epsilon
 
-    def create(self, input, observations):
-        self._params = self._createModelCallback(input, observations)
+    def createHypothesis(self):
+        self._params = self._createModelCallback(self._data)
 
-    def evaluate(self, input, observations):
-        return self._evaluateModelCallback(input, observations)
+    def evaluate(self):
+        return self._evaluateModelCallback(self._data, self._params, self._epsilon)
 
-def ransac(minModelParams, kmax=100):
-    pass;
+    def getParams(self):
+        return self._params
+
+def ransac(model, kmax=100):
+    '''
+    Ransac implmentation
+    :param model: of class RansacModel
+    :param kmax: max number of iterations
+    :return: maxinliers, best fit
+    '''
+
+    k = 0
+    maxInliers = -1
+    bestParams = None
+    while k < kmax:
+        k = k + 1
+
+        # Find initial hypothesis
+        model.createHypothesis()
+
+        # Evaluate all data
+        inliers = model.evaluate()
+
+        # Store best fit
+        if inliers > maxInliers:
+            maxInliers = inliers
+            bestParams = model.getParams()
+
+    return maxInliers, bestParams
 
 
 def line_with_noise(params, x, mu=0, sigma=5):
@@ -38,6 +72,45 @@ def line_with_noise(params, x, mu=0, sigma=5):
     return y
 
 
+def createLineModel(data):
+    '''
+    Samples the data randomly and builds line model from a minimum subset
+    :param data:
+    :return:
+    '''
+
+    i1 = random.randint(0, len(data) - 1)
+    i2 = random.randint(0, len(data) - 1)
+
+    X1 = data[i1]
+    X2 = data[i2]
+
+    m = (X2[1] - X1[1])/(X2[0] - X1[0])
+    b = m * X1[0] - X1[1]
+
+    return [m, b]
+
+def evaluateLineModel(data, params, epsilon):
+    '''
+    Evaluates all data with respect to a model hypothesis
+    :param data: data to fit
+    :param params: the model hypothesis
+    :param epsilon: min error to consider as a inlier
+    :return: number of inliers
+    '''
+
+    m, b = params
+    inliers = 0
+    for point in data:
+        y = m * point[0] + b
+
+        delta = abs(y - point[1])
+        if delta < epsilon:
+            inliers = inliers + 1
+
+    return inliers
+
+
 def testRANSAC():
     #############################
     # TEST RANSAC
@@ -52,8 +125,10 @@ def testRANSAC():
     # Observations
     y = line_with_noise(line_params, x, 0, 2)
 
+    points = np.array([x, y]).T
+    line_ransac = RansacModel(points, createLineModel, evaluateLineModel, 50)
 
-
+    return ransac(line_ransac)
 
 
 if __name__ == '__main__':
